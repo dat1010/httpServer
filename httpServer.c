1baffe6ed6083f7d5a174e21f98e66a3647246ec
tannerd <tannerd@mail.gvsu.edu>
2015-01-28 12:38:13 -0500

    importing from a file

--- httpServer.c
+++ httpServer.c
@@ -7,48 +7,91 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <err.h>
+#include <string.h>
+#include <time.h>
 
-char response[] = "HTTP/1.1 200 OK\r\n"
-"Content-Type: text/html; charset=UTF-8\r\n\r\n"
-"<doctype !html><html><head><title>Bye-bye baby bye-bye</title>"
-"<style>body { background-color: #111 }"
-"h1 { font-size:4cm; text-align: center; color: black;"
-" text-shadow: 0 0 2mm red}</style></head>"
-"<body><h1>Goodbye, world!</h1></body></html>\r\n";
+int lSize = 0;
+
+
+char *getHtmlFile(char fileName[]){
+    FILE *fp;
+    char *buffer;
+    
+    fp = fopen ( fileName , "rb" );
+    if( !fp ) perror(fileName),exit(1);
+    
+    fseek( fp , 0L , SEEK_END);
+    lSize = ftell( fp );
+    rewind( fp );
+    
+    buffer = calloc( 1, lSize+1 );
+    if( !buffer ) fclose(fp),fputs("memory alloc fails",stderr),exit(1);
+    
+    /* copy the file into the buffer */
+    if( 1!=fread( buffer , lSize, 1 , fp) )
+        fclose(fp),free(buffer),fputs("entire read fails",stderr),exit(1);
+    fclose(fp);
+    return buffer;
+}
+
+char* getTime(){
+    char buf[1000];
+    time_t now = time(0);
+    struct tm tm = *gmtime(&now);
+    strftime(buf, sizeof buf, "%a, %d %b %Y %H:%M:%S %Z", &tm);
+    return buf;
+}
+
+
+
 
 int main()
 {
-  int one = 1, client_fd;
-  struct sockaddr_in svr_addr, cli_addr;
-  socklen_t sin_len = sizeof(cli_addr);
 
-  int sock = socket(AF_INET, SOCK_STREAM, 0);
-  if (sock < 0)
-    err(1, "can't open socket");
+    int one = 1, client_fd;
+    struct sockaddr_in svr_addr, cli_addr;
+    socklen_t sin_len = sizeof(cli_addr);
+    char *buffer = getHtmlFile("index.html");
 
-  setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(int));
+    
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0)
+        err(1, "can't open socket");
 
-  int port = 8080;
-  svr_addr.sin_family = AF_INET;
-  svr_addr.sin_addr.s_addr = INADDR_ANY;
-  svr_addr.sin_port = htons(port);
+    setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(int));
 
-  if (bind(sock, (struct sockaddr *) &svr_addr, sizeof(svr_addr)) == -1) {
-    close(sock);
-    err(1, "Can't bind");
-  }
+    int port = 8080;
+    svr_addr.sin_family = AF_INET;
+    svr_addr.sin_addr.s_addr = INADDR_ANY;
+    svr_addr.sin_port = htons(port);
 
-  listen(sock, 5);
-  while (1) {
-    client_fd = accept(sock, (struct sockaddr *) &cli_addr, &sin_len);
-    printf("got connection\n");
-
-    if (client_fd == -1) {
-      perror("Can't accept");
-      continue;
+    if (bind(sock, (struct sockaddr *) &svr_addr, sizeof(svr_addr)) == -1) {
+        close(sock);
+        err(1, "Can't bind");
     }
 
-    write(client_fd, response, sizeof(response) - 1); /*-1:'\0'*/
-    close(client_fd);
+    listen(sock, 5);
+    while (1) {
+        client_fd = accept(sock, (struct sockaddr *) &cli_addr, &sin_len);
+        
+        if(client_fd < 0)
+        {
+            printf("Could not connect to Client.");
+            return 1;
+        }
+        
+        printf("Client Connected\n");
+        
+        printf("Time is: [%s]\n", getTime());
+
+        if (client_fd == -1) {
+            perror("Can't accept");
+            continue;
+        }
+        
+        send(client_fd, buffer, lSize,0); /*-1:'\0'*/
+        close(client_fd);
   }
+    
+    free(buffer);
 }
