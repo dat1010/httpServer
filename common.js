cebe38de55753719eadaa50f99b7071ab2686d72
tannerd <tannerd@mail.gvsu.edu>
2015-02-02 15:15:46 -0500

    ajax page loading

--- common.js
+++ common.js
@@ -1,6 +1,27 @@
-$(document).ready(function(){
-    $("#myTab a").click(function(e){
-        e.preventDefault();
-        $(this).tab('show');
-    });
-});
+function loadAndSetActive(file,element, active){
+
+// first remove all other active classes
+$("#Home").removeClass("active");
+$("#About").removeClass("active");
+
+$(active).addClass("active");
+$(element).load(file);
+
+return;
+
+}
+
+function homePage()
+{
+  $( "#pageBody" ).load( "home.html" );
+  $("#aboutP").removeClass("active");
+  $("#homeP").addClass("active");
+}
+
+function aboutPage()
+{
+  $( "#pageBody" ).load( "about.html" );
+  $("#homeP").removeClass("active");
+  $("#aboutP").addClass("active");
+
+}
